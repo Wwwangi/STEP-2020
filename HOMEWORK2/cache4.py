341b7a2718fb950276e783e6a7979d31a9d02a8d
Wu Wanqi <43989412+Wwwangi@users.noreply.github.com>
2020-05-22 21:39:47 +0900

    Add files via upload

---
+++ HOMEWORK2/cache4.py
@@ -0,0 +1,100 @@
+import sys
+import collections
+
+# Cache is a data structure that stores the most recently accessed N pages.
+# See the below test cases to see how it should work.
+class Cache:
+  # Initializes the cache.
+  # |n|: The size of the cache.
+  def __init__(self, n):
+    self.cache = collections.OrderedDict()
+    self.size = n
+
+  # Access a page and update the cache so that it stores the most
+  # recently accessed N pages. This needs to be done with mostly O(1).
+  # |url|: The accessed URL
+  # |contents|: The contents of the URL
+  def access_page(self, url, contents):
+    if url in self.cache.keys():
+      #The time comlexity for move_to_end function is O(1) since it's only an update of linked list (by looking at the source code)
+      self.cache.move_to_end(url)
+    else:
+      if len(self.cache.keys())>=self.size:
+        self.cache.pop(list(self.cache.keys())[0])
+      self.cache[url]=contents
+
+
+  # Return the URLs stored in the cache. The URLs are ordered
+  # in the order in which the URLs are mostly recently accessed.
+  def get_pages(self):
+    temp=list(self.cache.keys())
+    temp.reverse()
+    return temp
+
+
+# Does your code pass all test cases? :)
+def cache_test():
+  # Set the size of the cache to 4.
+  cache = Cache(4)
+  # Initially, no page is cached.
+  equal(cache.get_pages(), [])
+  # Access "a.com".
+  cache.access_page("a.com", "AAA")
+  # "a.com" is cached.
+  equal(cache.get_pages(), ["a.com"])
+  # Access "b.com".
+  cache.access_page("b.com", "BBB")
+  # The cache is updated to:
+  #   (most recently accessed)<-- "b.com", "a.com" -->(least recently accessed)
+  equal(cache.get_pages(), ["b.com", "a.com"])
+  # Access "c.com".
+  cache.access_page("c.com", "CCC")
+  # The cache is updated to:
+  #   (most recently accessed)<-- "c.com", "b.com", "a.com" -->(least recently accessed)
+  equal(cache.get_pages(), ["c.com", "b.com", "a.com"])
+  # Access "d.com".
+  cache.access_page("d.com", "DDD")
+  # The cache is updated to:
+  #   (most recently accessed)<-- "d.com", "c.com", "b.com", "a.com" -->(least recently accessed)
+  equal(cache.get_pages(), ["d.com", "c.com", "b.com", "a.com"])
+  # Access "d.com" again.
+  cache.access_page("d.com", "DDD")
+  # The cache is updated to:
+  #   (most recently accessed)<-- "d.com", "c.com", "b.com", "a.com" -->(least recently accessed)
+  equal(cache.get_pages(), ["d.com", "c.com", "b.com", "a.com"])
+  # Access "a.com" again.
+  cache.access_page("a.com", "AAA")
+  # The cache is updated to:
+  #   (most recently accessed)<-- "a.com", "d.com", "c.com", "b.com" -->(least recently accessed)
+  equal(cache.get_pages(), ["a.com", "d.com", "c.com", "b.com"])
+  cache.access_page("c.com", "CCC")
+  equal(cache.get_pages(), ["c.com", "a.com", "d.com", "b.com"])
+  cache.access_page("a.com", "AAA")
+  equal(cache.get_pages(), ["a.com", "c.com", "d.com", "b.com"])
+  cache.access_page("a.com", "AAA")
+  equal(cache.get_pages(), ["a.com", "c.com", "d.com", "b.com"])
+  # Access "e.com".
+  cache.access_page("e.com", "EEE")
+  # The cache is full, so we need to remove the least recently accessed page "b.com".
+  # The cache is updated to:
+  #   (most recently accessed)<-- "e.com", "a.com", "c.com", "d.com" -->(least recently accessed)
+  equal(cache.get_pages(), ["e.com", "a.com", "c.com", "d.com"])
+  # Access "f.com".
+  cache.access_page("f.com", "FFF")
+  # The cache is full, so we need to remove the least recently accessed page "c.com".
+  # The cache is updated to:
+  #   (most recently accessed)<-- "f.com", "e.com", "a.com", "c.com" -->(least recently accessed)
+  equal(cache.get_pages(), ["f.com", "e.com", "a.com", "c.com"])
+  print("OK!")
+
+# A helper function to check if the contents of the two lists is the same.
+def equal(list1, list2):
+  if len(list1) != len(list2):
+    return False
+  for i in range(len(list1)):
+    if list1[i] != list2[i]:
+      return False
+  return True
+
+if __name__ == "__main__":
+  cache_test()
